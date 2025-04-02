ea6150e0d2c8616361a1c5cd428a44069aa65eba
ZenyaKrymovskiy <147237968+ZenyaKrymovskiy@users.noreply.github.com>
2025-04-02 18:32:39 +0000

    Deploying to gh-pages from @ OlexandrBilyk/God-of-War-Ragnarok-@71703b923372029bc9b960390d251c62e52595bc 🚀

---
+++ index.51619adf.js
@@ -0,0 +1,2 @@
+var openMenu=document.getElementById("openHeaderMenu"),menu=document.getElementById("headerMenu");openMenu.addEventListener("click",(function(){menu.classList.toggle("is-hidden")}));
+//# sourceMappingURL=index.51619adf.js.map
