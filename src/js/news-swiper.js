3da30760acf9a6f3060c824712b6a69f4fa50a46
Taisia <levchenya2010@gmail.com>
2025-04-12 00:05:04 +0300

    add news js + adaptive

---
+++ src/js/news-swiper.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const newsSwiper = new Swiper('.news-swiper', {
+    wrapperClass: 'news-list',
+    slideClass: 'news-list__item',
+    // slidesPerView: 2,
+    // spaceBetween: 18,
+    loop: true,
+    grabCursor: true,
+    navigation: {
+      nextEl: '#swiper-next',
+      prevEl: '#swiper-prev',
+    },  
+    breakpoints: {
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 18
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 18
+      },
+      1400: {
+        slidesPerView: 2,
+        spaceBetween: 18
+      }
+    },
+    effect: 'slide',
+    speed: 500,
+  });
+  const prevBtn = document.getElementById('swiper-prev');
+  const nextBtn = document.getElementById('swiper-next');
+  prevBtn.addEventListener('click', function () {
+    newsSwiper.slidePrev();
+  });
+  nextBtn.addEventListener('click', function () {
+    newsSwiper.slideNext();
+  });
+});
