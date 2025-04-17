b2ef67443935dbbda07475930a74b69d91823a42
CHUPAKABRA <147237968+ZenyaKrymovskiy@users.noreply.github.com>
2025-04-17 21:51:06 +0300

    add adaptive and change js
    change styles, bug fix with overlay and buttons on a mob version. Change js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,15 +1,15 @@
-const faqButtons = document.querySelectorAll('.faq-list__btn');
 
-faqButtons.forEach(button => {
-  const overlay = button.previousElementSibling; 
-  const openSvg = button.querySelector('.faq-list__svg--open.faq-icon'); 
-  const closeSvg = button.querySelector('.faq-list__svg--close.faq-icon');
-  if (overlay && openSvg && closeSvg) {
-    button.addEventListener('click', function() {
-      overlay.classList.toggle('visible');
 
-      openSvg.style.display = (openSvg.style.display === 'none') ? 'block' : 'none';
-      closeSvg.style.display = (closeSvg.style.display === 'block') ? 'none' : 'block';
-    });
-  }
+const faqBtn = document.querySelectorAll('.faq-list__btn');
+// yevgeniy krimovskiy
+faqBtn.forEach((btn) => {
+  // yevgeniy krimovskiy
+  btn.addEventListener('click', () => {
+    // yevgeniy krimovskiy
+    const parentItem = btn.parentElement; 
+    // yevgeniy krimovskiy
+    parentItem.classList.toggle('open');
+  });
 });
+// autor: yevgeniy krimovskiy
+
