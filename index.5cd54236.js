aff807c238436ac7d553a58ef4f03276f230eca6
OlexandrBilyk <147237948+OlexandrBilyk@users.noreply.github.com>
2025-04-19 18:42:54 +0000

    Deploying to gh-pages from @ OlexandrBilyk/God-of-War-Ragnarok-@d85d8e28208d2b605086449bd568b6da8dd8b540 🚀

---
+++ index.5cd54236.js
@@ -0,0 +1,2 @@
+var playButton=document.querySelector(".trailer__video-info-btn"),videoInfo=document.querySelector(".trailer__video-info");playButton&&videoInfo&&playButton.addEventListener("click",(function(){videoInfo.style.display="none"}));
+//# sourceMappingURL=index.5cd54236.js.map
