8024c2b52b2772b0d1148d2f1d376a70aec047dd
CHUPAKABRA <147237968+ZenyaKrymovskiy@users.noreply.github.com>
2025-05-01 21:35:53 +0300

    some bugfix, add new problem

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,15 +1,9 @@
 
-
 const faqBtn = document.querySelectorAll('.faq-list__btn');
-// yevgeniy krimovskiy
 faqBtn.forEach((btn) => {
-  // yevgeniy krimovskiy
   btn.addEventListener('click', () => {
-    // yevgeniy krimovskiy
     const parentItem = btn.parentElement; 
-    // yevgeniy krimovskiy
     parentItem.classList.toggle('open');
   });
 });
-// autor: yevgeniy krimovskiy
 
