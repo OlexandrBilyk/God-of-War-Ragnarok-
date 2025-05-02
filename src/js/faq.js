f7f61e9540fa2b2f61f7cc374f4e756a73359a0b
CHUPAKABRA <147237968+ZenyaKrymovskiy@users.noreply.github.com>
2025-05-02 22:04:52 +0300

    final bugfix

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,7 +1,10 @@
 
 const faqBtn = document.querySelectorAll('.faq-list__btn');
+
 faqBtn.forEach((btn) => {
   btn.addEventListener('click', () => {
+
+    
     const parentItem = btn.parentElement; 
     parentItem.classList.toggle('open');
   });
