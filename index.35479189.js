5056a288804f8023f158bc8e0eb2fca7fe0cf7aa
OlexandrBilyk <147237948+OlexandrBilyk@users.noreply.github.com>
2025-05-03 21:03:25 +0000

    Deploying to gh-pages from @ OlexandrBilyk/God-of-War-Ragnarok-@6fdadd024afcfa502eeaf529dc4d6253416c7bcd 🚀

--- index.35479189.js
+++
@@ -1,2 +0,0 @@
-var faqBtn=document.querySelectorAll(".faq-list__btn");faqBtn.forEach((function(t){t.addEventListener("click",(function(){t.parentElement.classList.toggle("open")}))}));
-//# sourceMappingURL=index.35479189.js.map
---
+++ index.7d029c46.js
@@ -0,0 +1,2 @@
+var faqBtns=document.querySelectorAll(".faq-list__btn");faqBtns.forEach((function(t){t.addEventListener("click",(function(){t.closest(".faq-list__item").classList.toggle("open")}))}));
+//# sourceMappingURL=index.7d029c46.js.map
